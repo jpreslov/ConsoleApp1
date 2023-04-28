78d03895f94c250da39146b38cc6f0321ef7827f
jpreslov <jplov@protonmail.com>
2023-04-27 21:59:40 -0500

    Add project files.

---
+++ ConsoleApp1/Counter.cs
@@ -0,0 +1,18 @@
+﻿public class InputReader
+{
+    public void Greeting()
+    {
+        Console.WriteLine("Please enter the number you wish to count to:");
+    }
+
+    public void NumChecks(string countTo, string countBy)
+    {
+        if (countTo != null && countBy != null && !countTo.Contains(" ") && !countBy.Contains(" "))
+        {
+            long countToLong = long.Parse(countTo);
+            long countByLong = long.Parse(countBy);
+            Console.WriteLine("Count to " + countToLong + " in increments of " + countByLong);
+        }
+        else { Console.WriteLine("fuck you don't put spaces in that shit"); }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class CountingApp
+    {
+        static void Main()
+        {
+            InputReader input = new InputReader();
+            input.Greeting();
+
+            string countToUserInput = Console.ReadLine();
+
+            Console.WriteLine($"Okay, count to {countToUserInput}. Now enter a number to count by (incrementally).");
+
+            string countByUserInput = Console.ReadLine();
+
+            if (countToUserInput != null && countByUserInput != null)
+            {
+                input.NumChecks(countToUserInput, countByUserInput);
+            } 
+            else
+            {
+                Console.WriteLine("you fucked up");
+            }
+        }
+    }
+}
